75284ef6cc0a8623a1fd023a9c1f6e01bb63732b
goldcove <goldcove@users.noreply.github.com>
2020-09-14 18:38:48 +0200

    Reference to arm config file in /etc/arm
    Strange config file location in /opt/arm, and a symlinked file in /etc/arm. 
    This commit uses the /etc/arm/arm.yaml as the config file. This is in line with the standard config file location.

--- arm/config/config.py
+++ arm/config/config.py
@@ -4,7 +4,7 @@ import os
 # import re
 import yaml
 
-yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../..", "arm.yaml")
+yamlfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "/etc/arm", "arm.yaml")
 
 with open(yamlfile, "r") as f:
     cfg = yaml.load(f)
